0bee8899e73bc433c99ef880db8f4c2d570deee2
zantoichi <s.d.vallianos@gmail.com>
2023-02-22 03:35:05 +0200

    Adding lombok.config. Adding a Version property to documents for auditing to work as intended.

--- src/main/java/io/coding/tree/appointmate/booking/Appointment.java
+++ src/main/java/io/coding/tree/appointmate/booking/Appointment.java
@@ -5,17 +5,25 @@ import java.time.Duration;
 import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.lang.Nullable;
 
 @Document(Appointment.COLLECTION_NAME)
 @Value
 @Getter(AccessLevel.NONE)
+@RequiredArgsConstructor(onConstructor_ = @PersistenceCreator)
 public final class Appointment {
 
     static final String COLLECTION_NAME = "appointments";
 
+    @Version
+    @Nullable
+    private final Long version;
     @Id
     private final AppointmentId appointmentId;
     private final Instant appointmentTime;
@@ -23,8 +31,15 @@ public final class Appointment {
 
     private final AuditMetadata auditMetadata;
 
+    private Appointment(AppointmentId appointmentId, Instant appointmentTime, Duration appointmentDuration) {
+        this.version = null;
+        this.appointmentId = appointmentId;
+        this.appointmentTime = appointmentTime;
+        this.appointmentDuration = appointmentDuration;
+        this.auditMetadata = new AuditMetadata(null, null);
+    }
+
     public static Appointment withRandomId(Instant appointmentTime, Duration appointmentDuration) {
-        return new Appointment(AppointmentId.withRandomUUID(), appointmentTime, appointmentDuration,
-            new AuditMetadata(null, null));
+        return new Appointment(AppointmentId.withRandomUUID(), appointmentTime, appointmentDuration);
     }
 }
--- src/main/java/io/coding/tree/appointmate/business/Business.java
+++ src/main/java/io/coding/tree/appointmate/business/Business.java
@@ -5,18 +5,26 @@ import io.coding.tree.appointmate.common.PhoneNumber;
 import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.lang.Nullable;
 
 @Document(Business.COLLECTION_NAME)
 @Value
 @Getter(AccessLevel.NONE)
+@RequiredArgsConstructor(onConstructor_ = @PersistenceCreator)
 public final class Business {
 
     static final String COLLECTION_NAME = "businesses";
 
+    @Version
+    @Nullable
+    private final Long version;
     @Id
     private final BusinessId businessId;
     @Indexed(name = "businessName_index")
@@ -25,13 +33,26 @@ public final class Business {
     private final PhoneNumber phoneNumber;
     private final BusinessHours businessHours;
     private final Set<Staff> staff;
-    private final Set<BusinessServiceOffering> businessServiceOfferings;
+    private final Set<BusinessServiceOffering> serviceOfferings;
 
     private final AuditMetadata auditMetadata;
 
+    private Business(BusinessId businessId, String businessName, Industry industry, PhoneNumber phoneNumber,
+        BusinessHours businessHours, Set<Staff> staff, Set<BusinessServiceOffering> serviceOfferings) {
+        this.version = null;
+        this.businessId = businessId;
+        this.businessName = businessName;
+        this.industry = industry;
+        this.phoneNumber = phoneNumber;
+        this.businessHours = businessHours;
+        this.staff = staff;
+        this.serviceOfferings = serviceOfferings;
+        this.auditMetadata = new AuditMetadata(null, null);
+    }
+
     public static Business withRandomId(String businessName, Industry industry, PhoneNumber phoneNumber,
-        BusinessHours businessHours, Set<Staff> staff, Set<BusinessServiceOffering> businessServiceOfferings) {
+        BusinessHours businessHours, Set<Staff> staff, Set<BusinessServiceOffering> serviceOfferings) {
         return new Business(BusinessId.withRandomUUID(), businessName, industry,
-            phoneNumber, businessHours, staff, businessServiceOfferings, new AuditMetadata(null, null));
+            phoneNumber, businessHours, staff, serviceOfferings);
     }
 }
